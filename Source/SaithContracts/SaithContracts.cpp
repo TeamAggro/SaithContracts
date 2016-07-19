c633a54b5d9266fe86490171f0beb4091ac00e57
Ben Tam <ben.hs.tam@gmail.com>
2016-07-18 23:13:28 -0400

    Clearing old Unreal4 files

--- Source/SaithContracts/SaithContracts.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
-#include "SaithContracts.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SaithContracts, "SaithContracts" );
- 
